d773a09883344bb8b3f2eb03ca54a364515adc1b
Andrii Dovgopoluk <andrijdovgopoluk@gmail.com>
2025-12-23 10:09:13 +0200

    fixed task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,10 @@
 function formatMessage(message, maxLength) {
-  // Якщо довжина рядка не перевищує maxLength — повертаємо без змін
   if (message.length <= maxLength) {
     return message;
   }
 
-  // Якщо перевищує — обрізаємо та додаємо "..."
-  return message.slice(maxLength) + "...";
+  return message.slice(0, maxLength) + "...";
 }
-
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
